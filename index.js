a6fc326bc54ea700224a6f5331dc794926f9895d
Jane Park <jane.sypark@gmail.com>
2023-08-08 22:02:27 -0400

    loopingdone

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards (name, event){
+    const messages = [];
+    for (let i = 0; i < name.length; i++){
+    const message = `Thank you, ${name[i]}, for the wonderful ${event} gift!`;
+
+    messages.push(message);
+    }
+    return messages;
+}
+console.log(writeCards(['Tim', 'Jim', 'Bob'], 'birthday'));
+
+function countDown(){
+    let countDown = 10;
+    while (countDown >= 0){
+        console.log(countDown--);
+    }
+}
